959629ffd76ea2bf9422ae83a1ffd64c500f1074
Victor LE FLOCH <vilefloch@outlook.com>
2023-05-05 16:59:25 +0200

    plus pouvoir quitter page éditeur

--- CODE/VIEWS/editeur.php
+++ CODE/VIEWS/editeur.php
@@ -5,6 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script>
+        window.addEventListener("beforeunload", function(event) {
+            event.preventDefault(); // Annule la fermeture de la page
+            event.returnValue = ""; // Requiert une chaîne vide pour les navigateurs plus anciens
+            alert("Êtes-vous sûr de vouloir quitter cette page ?"); // Affiche l'alerte
+        });
+    </script>
 </head>
 <body>
     editeur
